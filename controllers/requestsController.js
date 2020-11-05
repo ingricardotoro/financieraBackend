2bd264aca51a7d0fea04710fe31a8c1bb3dc6cb7
Marvin Ricardo Toro <ingricardotoro@gmail.com>
2020-11-05 15:20:17 -0600

    Controlador de solicitudes

---
+++ controllers/requestsController.js
@@ -0,0 +1,109 @@
+const Request = require('../models/Request')
+    //const Contact = require('../models/contact')
+
+//funcion para listar todos los clientes
+const listRequest = async(req, res) => {
+
+    await Request.find({})
+        //.populate('personid')
+        .exec(function(err, requests) {
+            //en caso de obtener un error en la Busqueda
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                })
+            }
+
+            res.status(200).json({
+                ok: true,
+                msg: "Lista de Solicitudes",
+                requests
+            })
+
+            console.log(requests);
+        });
+}
+
+//funcion para crear nuevas Solicitudes
+const createRequest = async(req, res) => {
+
+    const {
+
+        customerId,
+        codeRequest,
+        typeLoan,
+        amount,
+        rate,
+        frequency,
+        quota,
+        totalInterest,
+        closingCost,
+        startDate,
+        sucursal,
+        details,
+        stateRequest,
+        createdBy,
+        approvedBy,
+        declinedBy
+
+    } = req.body
+
+
+    //creamos una instancia del objeto Persona
+    newRequest = new Request({
+        customerId,
+        codeRequest,
+        typeLoan,
+        amount,
+        rate,
+        frequency,
+        quota,
+        totalInterest,
+        closingCost,
+        startDate,
+        sucursal,
+        details,
+        stateRequest,
+        createdBy,
+        approvedBy,
+        declinedBy
+    })
+
+    try {
+
+        if (await newRequest.save()) {
+
+            //Cliente creado exitosamente
+            res.status(201).json({
+                ok: true,
+                msg: 'Solicitud creada exitosamente',
+                newCustomer
+            })
+        } else {
+            res.status(500).json({
+                ok: false,
+                msg: "Error creating New Request"
+            })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: "Error creating New Rquest",
+            errro: error
+        })
+    }
+
+}
+
+const deleteRequest = async(req, res) => {
+
+}
+
+const updateRequest = async(req, res) => {
+
+}
+
+module.exports = { createRequest, listRequest, deleteRequest, updateRequest }
