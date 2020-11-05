8c0ebf928aa5940d776400730f8e8900e1495b7e
Marvin Ricardo Toro <ingricardotoro@gmail.com>
2020-11-04 18:39:08 -0600

    creacion y listado de nuevos clientes

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -4,14 +4,103 @@ const Customer = require('../models/customer')
 //funcion para listar todos los clientes
 const listCustomer = async(req, res) => {
 
+    await Customer.find({})
+    //.populate('personid')
+    .exec(function(err, customers) {
+        //en caso de obtener un error en la Busqueda
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+
+        res.status(200).json({
+            ok: true,
+            msg: "Lista de Clientes",
+            customers
+        })
+
+        console.log(customers);
+    });
 }
 
 const listCustomerByName = async(req, res) => {
 
 }
 
+//funcion para crear nuevos clientes
 const createCustomer = async(req, res) => {
 
+    const {
+        
+        codeCustomer,
+        name,
+        lastname,
+        identidad,
+        gender,
+        rtn,
+        fec_nac,
+        phone1,
+        phone2,
+        email1,
+        email2,
+        city,
+        location,
+        photo,
+        Loanctive,
+        defaulter,
+        active
+    } = req.body
+
+
+        //creamos una instancia del objeto Persona
+        newCustomer = new Customer({
+            codeCustomer,
+            name,
+            lastname,
+            identidad,
+            gender,
+            rtn,
+            fec_nac,
+            phone1,
+            phone2,
+            email1,
+            email2,
+            city,
+            location,
+            photo,
+            Loanctive,
+            defaulter,
+            active
+        })
+
+        try {
+
+            if(await newCustomer.save()) {
+            
+                //Cliente creado exitosamente
+                res.status(201).json({
+                    ok: true,
+                    msg: 'Cliente creado exitosamente',
+                    newCustomer
+                })
+            }
+            else{
+                res.status(500).json({
+                    ok: false,
+                    msg: "Error creating New Customer"
+                })
+            }
+                
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({
+                ok: false,
+                msg: "Error creating New Customer"
+            })
+        }
+  
 }
 
 const deleteCustomer = async(req, res) => {
--- models/Customer.js
+++ models/Customer.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require('mongoose');
-const mongooseUniqueValidator = require('mongoose-unique-validator');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const customerSchema = Schema({
